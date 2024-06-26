3e8d207ca771869755fdc694fa850028b882bf9f
adan73 <167682529+adan73@users.noreply.github.com>
2024-06-26 05:47:47 +0300

    build two function one print the y-axis witch have numbers and lines the other function print-x print the monthes and the result for each one

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 
 window.onload = () => { 
   PrintPatientsList();
+  build_the_progress();
+  print_x();
 }
 
 
@@ -33,3 +35,58 @@ function PrintPatientsList(){
       .catch(error => console.error('Error fetching data:', error));
   }
   
+  
+  function build_the_progress(){
+    const yAxisContainer = document.querySelector('.y-axis');
+    
+        var x=0;
+        // Generate Y-axis numbers
+        for (let i = 100; i >= 10; i -= 10) {
+        
+            const yAxisLine = document.createElement('div');
+            yAxisLine.classList.add('y-axis-line');
+        
+            const yAxisNumber = document.createElement('span');
+            yAxisNumber.textContent = i;
+            yAxisNumber.classList.add('y-axis-number');
+
+            yAxisLine.appendChild(yAxisNumber);
+           
+            yAxisContainer.appendChild(yAxisLine);
+        }
+     
+}
+
+
+function print_x(){
+  const monthsContainer = document.querySelector('.months');
+    const resultsContainer = document.querySelector('.results');
+  const color = ["#003D32", "#00665F", "#35978F"];
+  const months = ["January", "February", "March"];
+  const results = {
+      "January": [178,30,90],
+      "February": [30, 209,259],
+      "March": [209, 120, 90]
+  };
+
+  months.forEach(month => {
+    const monthDiv = document.createElement('div');
+    monthDiv.textContent = month;
+    monthDiv.classList.add('month');
+    monthsContainer.appendChild(monthDiv);
+
+    // Generate results for each month
+    const monthResults = document.createElement('div');
+    monthResults.classList.add('month-results');
+    
+    results[month].forEach((result, index) => {
+        const resultDiv = document.createElement('div');
+        resultDiv.classList.add('result');
+        resultDiv.style.height = `${result}px`;
+        resultDiv.style.backgroundColor = color[index % color.length];
+        monthResults.appendChild(resultDiv);
+    });
+
+    resultsContainer.appendChild(monthResults);
+});
+}
